b40f4608f4bc048ae022b1533928816bbd557b32
ernestoOlaez <olaez_ernesto@hotmail.com>
2021-02-21 15:36:25 -0600

    Add an assetion to the example

--- BASICS/webDriverCommands/WebDriverCommands_1.java
+++ BASICS/webDriverCommands/WebDriverCommands_1.java
@@ -2,6 +2,7 @@ package webDriverCommands;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class WebDriverCommands_1 {
 
@@ -13,7 +14,7 @@ public class WebDriverCommands_1 {
 		
 		//Create a new instance for Chrome
 		WebDriver driver=new ChromeDriver();
-		String URL="https://demoqa.com";
+		String URL="https://demoqa.com/";
 		
 		//Launch the WebSite
 		driver.get(URL);
@@ -26,6 +27,20 @@ public class WebDriverCommands_1 {
 		//Printing the Title and titleLenght
 		System.out.println("The title of the page is:"+ Title);
 		System.out.println("The lenght of the title is:"+ titleLenght);
+		
+		//Storing actual URL in String variable
+		String actualURL=driver.getCurrentUrl();
+		if(actualURL.equals(URL))
+		{
+			System.out.println("Verification sucessful - The correct Url is opened.");
+		}
+		else
+		{
+			 System.out.println("Actual URL is : " + actualURL); 
+			 System.out.println("Expected URL is : " + URL);	
+		}
+		Assert.assertEquals(actualURL, URL);
+		System.out.println("Assert passed!...");
 
 	}
 
